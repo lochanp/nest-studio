eedd8e6f1ad37fadbd24c452c6f3446add4128f5
lochan07 <lochan@binaryveda.com>
2021-10-10 15:58:59 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Logo from '../assests/images/logo.png';
+const Header = () => {
+  return (
+    <div>
+      <img src={Logo}></img>
+    </div>
+  );
+};
+
+export default Header;
